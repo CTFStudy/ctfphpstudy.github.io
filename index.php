a2da8cbc646b1e7202b8b2d5c3b3f4902e8767c8
vstarl <87709783+VJiuMeng@users.noreply.github.com>
2021-08-16 19:44:29 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Study(一)</title>
+</head>
+<body>
+
+	<from actiom="check.php" method="POST">
+		<input type="text" name="username">
+		<input type="passwd" name="passwd">
+		<input type="submit" name="submit">
+	</from>
+
+</body>
+</html>
